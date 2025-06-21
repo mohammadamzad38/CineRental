da4f544c66e867c79cb8ab4f1a79e3478746d9ac
mohammadamzad38 <hossainamzad38@gmail.com>
2025-06-21 17:03:14 +0600

    restart

--- src/Components/MovieCart.jsx
+++ src/Components/MovieCart.jsx
@@ -1,9 +1,8 @@
-import Star from "../assets/star.svg";
+import Ratings from "./Ratings";
 
 import { getImageUrl } from "./Utils/Cine-utility";
 
-export default function MovieCart({movie}) {
- 
+export default function MovieCart({ movie }) {
   return (
     <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
       <img
@@ -15,11 +14,7 @@ export default function MovieCart({movie}) {
         <h3 className="text-xl mb-1">{movie.title}</h3>
         <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
         <div className="flex items-center space-x-1 mb-5">
-          <img src={Star} width="14" height="14" alt="" />
-          <img src={Star} width="14" height="14" alt="" />
-          <img src={Star} width="14" height="14" alt="" />
-          <img src={Star} width="14" height="14" alt="" />
-          <img src={Star} width="14" height="14" alt="" />
+          <Ratings />
         </div>
         <a
           className="primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -7,7 +7,7 @@ const MovieList = () => {
     <div className="content">
       <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-7">
         {movies.map((movie) => (
-          <MovieCart movie={movie} />
+          <MovieCart key={movie.id} movie={movie} />
         ))}
       </div>
     </div>
